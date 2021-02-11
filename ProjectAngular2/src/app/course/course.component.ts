d2637ae8056f96a6bedf957b4743ce21f88bdf8c
venkat <vch9391@gmail.com>
2021-02-11 18:16:13 -0500

    Added Course Component with String interpolation

---
+++ ProjectAngular2/src/app/course/course.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent {
+  title = "List of courses";
+  courses = ["courses1","courses2","courses3"];
+}
